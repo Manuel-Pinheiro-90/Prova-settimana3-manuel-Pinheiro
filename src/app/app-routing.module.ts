af74064cf7021bc4512f77991a9830c8c5de420a
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-07 10:21:57 +0200

    com guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestGuard } from './pages/auth/guest.guard';
+import { AuthGuard } from './pages/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
-   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-    { path: 'utente', loadChildren: () => import('./pages/utente/utente.module').then(m => m.UtenteModule) },
-     { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
-  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) }];
+  { path: '', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule),
+    canActivate: [GuestGuard],
+  canActivateChild: [GuestGuard],
+   },
+
+
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+
+
+
+  { path: 'utente', loadChildren: () => import('./pages/utente/utente.module').then(m => m.UtenteModule),
+    canActivate:[AuthGuard],
+  canActivateChild: [AuthGuard]
+   },
+
+
+  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule),
+    canActivate:[AuthGuard],
+  canActivateChild: [AuthGuard]
+   },
+
+
+
+  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule),
+     canActivate:[AuthGuard],
+    canActivateChild: [AuthGuard] }
+
+
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
